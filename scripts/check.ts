ef9b9656bb35f74f525508de6401b66cbbf3eb9c
udaykumardontha-star <udaykumardontha@gmail.com>
2026-06-21 22:04:01 +0530

    Remove test scripts to fix Vercel build

--- scripts/check.ts
+++
@@ -1,7 +0,0 @@
-import { db } from "../db/connection";
-import { sql } from "drizzle-orm";
-async function run() {
-  const job = await db.all(sql`SELECT file_name, total_pages, extracted_questions, failed_pages_json FROM import_jobs WHERE id = 32`);
-  console.log("Job 32:", job);
-}
-run();
